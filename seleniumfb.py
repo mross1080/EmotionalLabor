568f0fda18d00d8cc5b6bc9a38e6fb9a252cbeb7
Matt Ross <mattross1080@gmail.com>
2020-01-23 20:36:30 -0500

    Adds ability to properly scrape facebook photos for tagers

--- seleniumfb.py
+++ seleniumfb.py
@@ -2,20 +2,27 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 import os
-taggee_class = "taggee"
+# taggee_class = "taggee"
+taggee_class = "_hli"
 
 
 class TagFinder():
     def __init__(self):
-        self.in_between_wait = .3
+        self.in_between_wait = .05
         self.driver = self.setup_selenium_driver()
         self.taggees = {}
+        self.successful_retrievals = 0
+        self.first_url = ""
 
     def setup_selenium_driver(self):
         usr = os.getenv('fbusername')
         pwd = os.getenv('fbpassword')
-        driver = webdriver.Firefox(
-            executable_path='/Users/mattross/webdriver/gecko/v0.26.0/geckodriver-v0.26.0-macos/geckodriver')
+        PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+        DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+
+        driver = webdriver.Chrome(executable_path=DRIVER_BIN)
+        # driver = webdriver.Firefox(
+        #     executable_path='/Users/mattross/webdriver/gecko/v0.26.0/geckodriver-v0.26.0-macos/geckodriver')
 
         # driver = webdriver.Chrome()
         driver.get('https://www.facebook.com/')
@@ -43,45 +50,88 @@ class TagFinder():
             try:
                 tager = driver.find_element_by_class_name(taggee_class).text
                 if tager is not None:
-                    return tager
+
+                    try:
+                        tag_date = driver.find_element_by_class_name("_39g5").text
+                        if not self.taggees[tager]["last_tagged"]:
+                            self.taggees[tager]["last_tagged"] = tag_date
+                        self.taggees[tager]["first_tagged"] = tag_date
+                    except Exception as e:
+                        # print(e)
+                        print("")
+                        # print("could not get date ")
+
+
+                    # Manage Current Speed Of Timer
+                    if self.successful_retrievals < 6:
+                        self.successful_retrievals += 1
+                    else:
+                        # print("Retrieved 6 photos in a row successfully, resetting sleep timer")
+                        self.successful_retrievals = 0
+                        self.in_between_wait = .05
+
+                    if driver.current_url != self.first_url:
+                        return tager
+                    else:
+                        self.stop_iteration_and_display_data()
             except Exception as e:
 
-                print("Could not load page fast enough, trying again {}".format(e))
+                # print("Could not load page fast enough, trying again {}".format(e))
                 self.in_between_wait += .1
-                print("increasing global wait time to {}".format(self.in_between_wait))
+                # print("increasing global wait time to {}".format(self.in_between_wait))
                 sleep(self.in_between_wait)
+                driver.find_element_by_css_selector('body').send_keys(Keys.ARROW_RIGHT)
 
     def find_all_tagged_photos(self):
 
         driver = self.driver
-        driver.get("https://www.facebook.com/Ross.Ross.1080/photos")
-
+        profile_url = "https://www.facebook.com/Ross.Ross.1080/photos"
+        # profile_url = "https://www.facebook.com/steviedunbardude/photos"
+        # profile_url = "https://www.facebook.com/morgan.mueller.14/photos"
+        driver.get(profile_url)
+        #driver.get("https://www.facebook.com/dana.elkis/photos")
+        sleep(.5)
+        driver.find_element_by_css_selector('body').click()
         tags = driver.find_elements_by_class_name('fbPhotoStarGridElement')
+        sleep(1)
         tags[0].click()
 
         sleep(1)
         print("starting iteration ")
-        for i in range(2000):
+
+
+        for i in range(300):
+
             tager = self.get_name_of_tagger(driver)
 
+            if not self.first_url:
+                self.first_url = driver.current_url
             if tager not in self.taggees.keys():
-                self.taggees[tager] = 1
-                print("You have been tagged by {} ".format(tager))
+                self.taggees[tager] = {"tag_count": 1, "first_tagged": "", "last_tagged": ""}
+
+
+                print("You have been tagged by {} count is at {}".format(tager,len(self.taggees.keys())))
             else:
-                self.taggees[tager] += 1
+                self.taggees[tager]["tag_count"] += 1
             # print("You have been tagged by {} {}  times ".format(tager,taggees[tager]))
             driver.find_element_by_css_selector('body').send_keys(Keys.ARROW_RIGHT)
             sleep(self.in_between_wait)
 
-        print("Done")
-        input('Press anything to quit')
-        driver.quit()
-        print("Finished")
+        self.stop_iteration_and_display_data()
+
+    def stop_iteration_and_display_data(self):
+
+        self.driver.quit()
 
         print("Done Iterating through FB Photos, displaying counts now.....\n\n\n\n")
+        sleep(1)
 
         for tager in self.taggees.keys():
-            print("You have been tagged by {} {}  times ".format(tager, self.taggees[tager]))
+            print("You have been tagged by {} , {} times ".format(tager, self.taggees[tager]["tag_count"]))
+            print("The first time you were tagged by them was {}".format(self.taggees[tager]["first_tagged"]))
+            print("The last time you were tagged by them was {}".format(self.taggees[tager]["last_tagged"]))
+            print("-----------------------------------------------------------------------------")
+        exit()
 
 
 def main():
